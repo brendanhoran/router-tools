57d1d03534c1fbda8cb6cc1858bab7626413e7c5
Brendan Horan <brendan@horan.hk>
2017-05-03 19:17:52 +0800

    updating to use syspatch

--- shell/errata-checker.sh
+++ shell/errata-checker.sh
@@ -1,29 +1,31 @@
 #!/bin/ksh
 # Description :
-#  A simple script designed to run from crontab to check for new errata.
+#  A simple script designed to run from crontab to check for new syspatchs.
 
-obsd_version=`sysctl kern.osrelease | cut -c 16-18 | tr -d '.'`
+# Set the following :
 # Where to send email report to
-email_rcpt="brendan@horan.hk"
+email_rcpt="admin@basstech.net"
+# end settings
 
-
-errata=`links -dump http://www.openbsd.org/errata$obsd_version.html | grep FIX`
-applied_errata=`ls -l /root/patches/$obsd_version/ | awk '{print $9}'`
+applied_errata=`/usr/sbin/syspatch -l`
+obsd_version=`sysctl kern.osrelease | cut -c 16-18`
 mail_body="/tmp/errata-chk"
 
 echo "----------------------------------------------------------------" > $mail_body
-echo "Checking errata for $obsd_version" >> $mail_body
-echo " " >> $mail_body
+echo "Checking new patches for $obsd_version" >> $mail_body
 echo "----------------------------------------------------------------" >> $mail_body
-echo "Available errata patches upstream :" >> $mail_body
-echo " " >> $mail_body
-printf "$errata\n" >> $mail_body
-echo " " >> $mail_body
+echo "Available patches upstream :" >> $mail_body
+errata=`/usr/sbin/syspatch -c`
+if [ $? != 0 ]; then
+  printf "$errata\n" >> $mail_body
+else
+  printf "No new patches to install\n" >> $mail_body
+fi
 echo "----------------------------------------------------------------" >> $mail_body
-echo "Applied errata patches locally :" >> $mail_body
+echo "Applied patches :" >> $mail_body
 printf "$applied_errata\n" >> $mail_body
 echo "----------------------------------------------------------------" >> $mail_body
 
-cat $mail_body | mail -s "OpenBSD errata check report" $email_rcpt
+cat $mail_body | mail -s "OpenBSD patches check report" $email_rcpt
 
 rm $mail_body
