9880d633c0640fbf16cdbc794f446a9bd40f8bc3
Brendan Horan <brendanhoran@basstech.net>
2017-05-22 12:15:31 +0800

    fixed patch check logic

--- shell/syspatch-checker.sh
+++ shell/syspatch-checker.sh
@@ -4,26 +4,26 @@
 
 # Set the following :
 # Where to send email report to
-email_rcpt="your@email-address.net"
-# end settings
+email_rcpt=""
 
-applied_errata=`/usr/sbin/syspatch -l`
+
+applied_patches=`/usr/sbin/syspatch -l`
 obsd_version=`sysctl kern.osrelease | cut -c 16-18`
-mail_body="/tmp/errata-chk"
+mail_body="/tmp/patches-chk"
 
 echo "----------------------------------------------------------------" > $mail_body
 echo "Checking new patches for $obsd_version" >> $mail_body
 echo "----------------------------------------------------------------" >> $mail_body
 echo "Available patches upstream :" >> $mail_body
-errata=`/usr/sbin/syspatch -c`
-if [ $? != 0 ]; then
-  printf "$errata\n" >> $mail_body
+sys_patches=`/usr/sbin/syspatch -c`
+if [ -n "$sys_patches" ]; then
+  printf "$sys_patches\n" >> $mail_body
 else
   printf "No new patches to install\n" >> $mail_body
 fi
 echo "----------------------------------------------------------------" >> $mail_body
 echo "Applied patches :" >> $mail_body
-printf "$applied_errata\n" >> $mail_body
+printf "$applied_patches\n" >> $mail_body
 echo "----------------------------------------------------------------" >> $mail_body
 
 cat $mail_body | mail -s "OpenBSD patches check report" $email_rcpt
